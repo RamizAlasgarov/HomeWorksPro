abb2ad79f50e2a43a82d8c1c989282e40d7cf036
ramiz <ramiz90151@gmail.com>
2024-04-14 10:18:12 +0200

    .

--- src/main/java/_09_04_24/TwoDimensionalArray.java
+++ src/main/java/_09_04_24/TwoDimensionalArray.java
@@ -9,62 +9,33 @@ public class TwoDimensionalArray {
                                     {14, 19, 25, 26, 27},
                                     {15, 21, 28, 33, 36},
                                     {24, 29, 44, 55, 66}};
-        findNumberTwo(array,25);
+        numInMatrix(array,25);
 
-
-//        for (int i = 0; i < array.length; i++) {
-//            System.out.println(array[i]);
-//        }
-//        System.out.println(findNumber(array, 30));
     }
 
-    /**
-     * @param array
-     * @param number
-     * @return
-     */
-//    public static boolean findNumber(int[][] array, int number) {
-//        boolean isFinded = false;
-//        for (int i = 0; i < array.length; i++) {
-//            for (int j = 0; j < array[i].length; j++) {
-//                if (array[i][j] == number) {
-//                    return !isFinded;
-//                }
-//            }
-//        }
-//        return isFinded;
-//    }
 
-    public static void findNumberTwo(int[][] array, int number) {
-        int counter = 1;
-        int seconCounter = 1;
-        int startCount = array[4][0];
-        int endCount = array[0][4];
+    public static boolean numInMatrix (int[][] matrix, int findMe){
+        int count = 0;
 
-//        for (int i = end; i > array.length/2; i--) {
-//                if(end < number){
-//                        end = array[4][0+1];
-//                    if(end > number){
-//                        end = array[4-1][0+1];
-//                    }
-//
-//                    }
-//            if(end == number){
-//                System.out.println(end);
-//                }
-//        }
-        for (int i = startCount; i > endCount; i++) {
-            if(number > startCount) {
-                startCount = array[4][counter];
-
-
-                if (number < startCount) {
-                    startCount = array[4 - seconCounter][counter];
-                }
-                if (number == startCount) {
-                    System.out.println(number);
+        for (int i = 0; i < matrix.length ; i++) {
+            if (matrix[i][matrix[i].length-1]<findMe){
+                count++;
+                continue;
+            }
+            for (int j = matrix[i].length-1; j >=0; j--) {
+                count++;
+                if (matrix[i][j]==findMe){
+                    System.out.println(count);
+                    return true;}
+                if (matrix[i][j]<findMe) {
+                    j++;
+                    i++;
                 }
             }
+        }
+        System.out.println(count);
+        return false;
+    }
 
         }
 
