97d355cd09cf2223a934e34a873640b43409d430
ramiz <ramiz90151@gmail.com>
2024-04-24 14:33:08 +0200

    update

--- src/main/java/doTasks/_2024_04_17/taski/withHashMap/MapTask.java
+++ src/main/java/doTasks/_2024_04_17/taski/withHashMap/MapTask.java
@@ -1,24 +1,23 @@
 package doTasks._2024_04_17.taski.withHashMap;
 
 
-
 import java.util.*;
 
 import doTasks._2024_04_17.taski.*;
 
 public class MapTask {
-        //Count circles of a given color:
+    //Count circles of a given color:
     //Сосчитайте круги заданного цвета:
     public static Map<Color, Integer> countCirclesOfColor(List<Figure> figures) {
         Map<Color, Integer> colorCount = new HashMap<>();
-        for (Figure f : figures){
-            if(f instanceof Circle){
+        for (Figure f : figures) {
+            if (f instanceof Circle) {
                 Circle circle = (Circle) f;
                 Color color = circle.getColor();
-                if(colorCount.containsKey(color)){
-                    colorCount.put(color,colorCount.get(color)+1);
-                }else {
-                    colorCount.put(color,1);
+                if (colorCount.containsKey(color)) {
+                    colorCount.put(color, colorCount.get(color) + 1);
+                } else {
+                    colorCount.put(color, 1);
                 }
             }
         }
@@ -42,12 +41,12 @@ public class MapTask {
         Map<Color, Integer> radiusSum = new HashMap<>();
 
         for (Figure figure : figures) {
-            if(figure instanceof Circle){
+            if (figure instanceof Circle) {
 
                 Circle circle = (Circle) figure;
                 int sum = circle.getRadius();
-                if(circle.getColor().equals(color)){
-                    radiusSum.put(circle.getColor(),sum);
+                if (circle.getColor().equals(color)) {
+                    radiusSum.put(circle.getColor(), sum);
                 }
             }
         }
@@ -55,12 +54,29 @@ public class MapTask {
     }
 
     //Find the rectangle with the largest diagonal to perimeter ratio:
+
     /**
-     Найдите прямоугольник с наибольшим отношением диагонали к периметру:
+     * Найдите прямоугольник с наибольшим отношением диагонали к периметру:
      */
     public static Map<Rectangle, Double> findRectangleWithLargestDiagonalToPerimeterRatio(List<Figure> figures) {
         Map<Rectangle, Double> rectangleRatios = new HashMap<>();
+        List<Rectangle> rectangleList = new ArrayList<>();
+        List<Circle> circleList = new ArrayList<>();
+        for (Figure figure : figures) {
+            if (figure instanceof Rectangle) {
+                rectangleList.add((Rectangle) figure);
+            }
+            if (figure instanceof Circle) {
+                circleList.add((Circle) figure);
+            }
+        }
+        for (Rectangle rectangle : rectangleList) {
+            int perimetr = 2 * (rectangle.getA() + rectangle.getB());
+            int a = rectangle.getA();
+            int b = rectangle.getB();
 
+
+        }
         return rectangleRatios;
     }
 
@@ -89,7 +105,7 @@ public class MapTask {
 
                 int radius = circle.getRadius();
 
-                if(radius == a || radius == b || radius == c){
+                if (radius == a || radius == b || radius == c) {
                     result.put(circle, triangle);
 //                } else {
 //                    result.put(circle, false);
@@ -101,29 +117,34 @@ public class MapTask {
     }
 
 
-
     //Determine if there's a rectangle and triangle of the same color:
     //Определите, есть ли прямоугольник и треугольник одного цвета
-    public static Map<String, Boolean> hasRectangleAndTriangleOfSameColor(List<Figure> figures) {
-        Map<String,Boolean> sameColor = new HashMap<>();
+    public static Map<Color, Boolean> hasRectangleAndTriangleOfSameColor(List<Figure> figures) {
+        Map<Color, Boolean> sameColor = new HashMap<>();
+        List<Rectangle> rectangles = new ArrayList<>();
+        List<Triangle> triangles = new ArrayList<>();
         for (Figure figure : figures) {
-            if(figure instanceof Rectangle ){
-                Rectangle rectangle = (Rectangle) figure;
-                String color = String.valueOf(rectangle.getColor());
-                boolean a = sameColor.getOrDefault(color,false);
-                boolean b = sameColor.getOrDefault(color,false);
-
-                if(a && b ){
-                    continue;
-                }
-                if (figure instanceof Rectangle) {
-                    sameColor.put(color, true);
+            if (figure instanceof Rectangle) {
+                rectangles.add(((Rectangle) figure));
             }
-                if (figure instanceof Triangle) {
+            if (figure instanceof Triangle) {
+                triangles.add((Triangle) figure);
+            }
+        }
+        for (Rectangle rectangle : rectangles) {
+            for (Triangle triangle : triangles) {
+                Color color = rectangle.getColor();
+                Color color1 = triangle.getColor();
+                if (color1.equals(color)) {
                     sameColor.put(color, true);
                 }
+                System.out.print(triangle);
+                System.out.print(rectangle);
+//                else {
+//                    sameColor.put(color,false);
+//                }
+            }
         }
-    }
         return sameColor;
-}
+    }
 }
