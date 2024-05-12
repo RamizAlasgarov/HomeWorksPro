ace4de617724fdbc0f2e97489d8dc3f89240aa55
ramiz <ramiz90151@gmail.com>
2024-05-12 21:43:16 +0200

    SimpleHouseCollectionTasks ended

--- src/main/java/_2024_05_07/testCollection/SimpleHouseCollectionTasks.java
+++ src/main/java/_2024_05_07/testCollection/SimpleHouseCollectionTasks.java
@@ -6,10 +6,13 @@ import java.util.List;
 import java.util.Set;
 
 public class SimpleHouseCollectionTasks {
-    
+
     // Задание 1: Вернуть количество подъездов во всех домах (List)
     public static int getTotalEntrances(List<House> houses) {
         int totalEntrances = 0;
+        for (House house : houses) {
+            totalEntrances += house.entrance;
+        }
 
         return totalEntrances;
     }
@@ -17,6 +20,13 @@ public class SimpleHouseCollectionTasks {
     // Задание 2: Вернуть список всех этажей во всех домах, где имеется лифт (List и Set для исключения повторений)
     public static List<Integer> getFloorsWithElevators(List<House> houses) {
         Set<Integer> uniqueFloors = new HashSet<>();
+        for (House house : houses) {
+            for (Flat flat : house.flats) {
+                if (house.hasElevator) {
+                    uniqueFloors.add(flat.floor);
+                }
+            }
+        }
 
         return new ArrayList<>(uniqueFloors);
     }
@@ -24,20 +34,32 @@ public class SimpleHouseCollectionTasks {
     // Задание 3: Вернуть список домов, в которых более 50 квартир (List)
     public static List<House> getLargeHouses(List<House> houses) {
         List<House> largeHouses = new ArrayList<>();
-
+        for (House house : houses) {
+            if (house.flats.size() > 50) {
+                largeHouses.add(house);
+            }
+        }
         return largeHouses;
     }
 
     // Задание 4: Проверить, есть ли хотя бы один дом без лифта (List)
     public static boolean hasHouseWithoutElevator(List<House> houses) {
-
+        for (House house : houses) {
+            if (!house.isHasElevator()) {
+                return true;
+            }
+        }
         return false;
     }
 
     // Задание 5: Вернуть общее количество комнат во всех домах (List)
     public static int getTotalRooms(List<House> houses) {
         int totalRooms = 0;
-
+        for (House house : houses) {
+            for (Flat flat : house.flats) {
+                totalRooms += flat.roomList.size();
+            }
+        }
         return totalRooms;
     }
 
